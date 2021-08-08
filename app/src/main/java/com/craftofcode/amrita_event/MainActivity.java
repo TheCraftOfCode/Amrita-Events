128594aa805906e79672835de53817c72378c2e0
sridhar-5 <kv.sridharsai@gmail.com>
2021-08-08 15:59:52 +0530

    initial commit

---
+++ app/src/main/java/com/craftofcode/amrita_event/MainActivity.java
@@ -0,0 +1,14 @@
+package com.craftofcode.amrita_event;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
