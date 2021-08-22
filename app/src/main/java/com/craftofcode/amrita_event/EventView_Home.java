df6f831b3a95326d2e81131bf48d187c00c1cf1e
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-22 09:36:23 +0530

    Added event layout

--- app/src/main/java/com/craftofcode/amrita_event/EventView_Home.java
+++ app/src/main/java/com/craftofcode/amrita_event/EventView_Home.java
@@ -10,5 +10,6 @@ public class EventView_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view_home);
+
     }
 }
