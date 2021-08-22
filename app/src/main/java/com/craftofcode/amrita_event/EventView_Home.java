88c5ca617920846c897db43da47e346713ddd556
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-22 11:47:02 +0530

    Added event class

--- app/src/main/java/com/craftofcode/amrita_event/EventView_Home.java
+++ app/src/main/java/com/craftofcode/amrita_event/EventView_Home.java
@@ -1,15 +1,36 @@
 package com.craftofcode.amrita_event;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class EventView_Home extends AppCompatActivity {
+
+    private Context context;
+    private ArrayList<Event_Details> eventDetailsArrayList;
+
+
+    public EventView_Home(Context context, ArrayList<Event_Details> eventDetailsArrayList) {
+        this.context = context;
+        this.eventDetailsArrayList = eventDetailsArrayList;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view_home);
+
+        Bundle importFromCards = getIntent().getExtras();
+        int position = importFromCards.getInt("position");
+
+        TextView EventTitle, EventDescription,EventDate,EventTime,EventPhone;
+
+
+
 
     }
 }
