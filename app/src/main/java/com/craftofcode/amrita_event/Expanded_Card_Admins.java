e8d2ab62e7a7a96d18755ecee3635246e1e49947
sridhar-5 <kv.sridharsai@gmail.com>
2021-08-30 23:14:22 +0530

    added admin side interface

--- app/src/main/java/com/craftofcode/amrita_event/Expanded_Card_Admins.java
+++ app/src/main/java/com/craftofcode/amrita_event/Expanded_Card_Admins.java
@@ -1,20 +1,15 @@
 package com.craftofcode.amrita_event;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Expanded_Card_Admins extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, Card_list_View_Admins.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_expanded_card_admins);
     }
-
 
 }
