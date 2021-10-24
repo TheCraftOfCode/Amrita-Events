5c9a5120289a6558561b237efadffdd8aee3dff0
sridhar-5 <kv.sridharsai@gmail.com>
2021-10-24 20:49:43 +0530

    Converted the Admin side Api calls Lib to Volley From Fast Android

--- app/src/main/java/com/craftofcode/amrita_event/Card_list_View_Admins.java
+++ app/src/main/java/com/craftofcode/amrita_event/Card_list_View_Admins.java
@@ -1,31 +1,39 @@
 package com.craftofcode.amrita_event;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.common.Priority;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interceptors.HttpLoggingInterceptor;
-import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.craftofcode.amrita_event.adapter.EventListAdapter;
-import com.craftofcode.amrita_event.apiModel.AdminCardModel;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.jacksonandroidnetworking.JacksonParserFactory;
+import com.craftofcode.amrita_event.apiModel.MySingleton;
 
 import org.json.JSONArray;
 
-import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.LinkedList;
-
-import okhttp3.OkHttpClient;
+import java.util.Map;
 
 public class Card_list_View_Admins extends AppCompatActivity {
+
+    Cache cache;
+    Network network;
+    RequestQueue requestQueue;
+
     public EventListAdapter adapter;
     public String[] Title = {"Peppy paneer Pizza", "Paneer Makhni Pizza", "Cheese Burst Pizza", "Corn Pizza","papperoni pizza","farm house pizza", "vegie deilight pizza","chicken pizza", "tandoori pizza","custom pizza"};
     public String[] Club = {"Peppy paneer Pizza", "Paneer Makhni Pizza", "Cheese Burst Pizza", "Corn Pizza","papperoni pizza","farm house pizza", "vegie deilight pizza","chicken pizza", "tandoori pizza","custom pizza"};
@@ -55,21 +63,47 @@ public class Card_list_View_Admins extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_list_view_admins);
 
-        // Adding an Network Interceptor for Debugging purpose :
-        OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
-                .addNetworkInterceptor(new HttpLoggingInterceptor())
-                .build();
+        // setting up the Request Queue
+        SettingUpRequestQueue();
 
+        SharedPreferences TOKEN = getSharedPreferences("TOKEN", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = TOKEN.edit();
 
-        AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
+        //pushing token to shared preference
+        edit.putString("user-auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTIxM2YyNmUzNmZhMjAwMDRlZjM0MDUiLCJ1c2VybmFtZSI6IkNCLkVOLlU0Q1NFMTkwNjMiLCJpYXQiOjE2MzUwODczODV9.EIfz22nKX7B6cCMgJ8eVbWDK2SPmRr38JJdFjPzgwg0");
+        edit.commit();
 
-        // setting the JacksonParserFactory below
-        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+        // Api call is being made Here
+        String AdminUsersEventsEndpoint = "https://amrita-events.herokuapp.com/api/admin-users-portal";
 
-        GetRequestToAdminCardView();
+        JsonArrayRequest EventCardRequest = new JsonArrayRequest
+                (Request.Method.GET, AdminUsersEventsEndpoint, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        System.out.println(response);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                SharedPreferences TOKEN = getSharedPreferences("TOKEN", Context.MODE_PRIVATE);
+                params.put("user-auth-token", TOKEN.getString("user-auth-token","Theif..!"));
+                return params;
+            }
+        };
+
+        //adding the request to Queue
+        MySingleton.getInstance(this).addToRequestQueue(EventCardRequest);
 
         recyclerView = findViewById(R.id.recyclerView);
-
         for(int i = 0; i< Title.length; i++){
             EventTitle.addLast(Title[i]);
             EventImage.addLast(EventImages[i]);
@@ -81,30 +115,11 @@ public class Card_list_View_Admins extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private LinkedList<AdminCardModel> GetRequestToAdminCardView() {
-        final LinkedList<AdminCardModel>[] Events = new LinkedList[1];
-        AndroidNetworking.get("https://amrita-events.herokuapp.com/api/admin-users-portal")
-                .addHeaders("user-auth-token", "")
-                .setPriority(Priority.HIGH)
-                .setTag("CardRequest")
-                .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Gson gson = new Gson();
-                        String JsonResponse = response.toString();
-                        Type listType = new TypeToken<LinkedList<AdminCardModel>>(){}.getType();
-                        Events[0] = gson.fromJson(JsonResponse, listType);
-                        System.out.println(Events[0]);
-                    }
-                    @Override
-                    public void onError(ANError error) {
-                        // handle error
-                        Log.d("message", "Error as usual");
-                    }
-                });
-        return Events[0];
+    private void SettingUpRequestQueue() {
+        cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); //1Mb cap
+        network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
     }
-
 
 }
--- app/src/main/java/com/craftofcode/amrita_event/apiModel/AdminCardModel.java
+++
@@ -1,9 +0,0 @@
-package com.craftofcode.amrita_event.apiModel;
-
-public class AdminCardModel {
-    public String _id;
-    public String ImageUrl;
-    public String Title;
-    public String OrganizingClub;
-    public String Date;
-}
